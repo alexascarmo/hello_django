23ffe8ca5e62cab000f99440ae39d552e6df1896
Alex Carmo(ACBZ) <Alex_Carmo@corpnet.asus>
2020-10-23 14:12:51 -0300

    nova url com somatorio de valores

--- core/views.py
+++ core/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 
 def hello(request, nome, idade):
     return HttpResponse('<h1>Hello {} de {} anos<h1>'.format(nome, idade))
+
+def soma(request, num1, num2):
+    total = num1 + num2
+    return HttpResponse('A soma do numero {} com o numero {} é igual a {}'.format(num1, num2, total))
